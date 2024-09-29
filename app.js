c43677a0f787e913709e4c8598142352093e8d9f
qbbo <qbbo@outlook.sg>
2024-09-30 07:01:09 +0800

    添加 Socket.IO 实时通信功能
    - 引入 Socket.IO 依赖
    - 创建 Socket.IO 服务器实例
    - 实现基本的连接、消息处理和断开连接逻辑
    - 添加 'updateMessage' 事件处理

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+
 const userRoutes = require('./routes/userRoutes');
 const photoRoutes = require('./routes/photoRoutes');
 const messageRoutes = require('./routes/messageRoutes');
@@ -10,6 +13,12 @@ const middleware = require('./utils/middleware');
 const app = express();
 
 app.use(cors());
+
+const server = http.createServer(app);
+const io =new Server(server, {
+  cors: { origin: "*" }
+})
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(middleware.authMiddleware);
@@ -20,7 +29,21 @@ app.use('/api/v2/photos', photoRoutes);
 app.use('/api/v2/messages', messageRoutes);
 app.use('/api/v2/search', searchRoutes);
 
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  // 接收来自客户端的消息
+  socket.on('updateMessage', (msg) => {
+    // 可以将消息广播给所有客户端
+    io.emit('updateMessage', 'server:' + msg);
+  });
+
+  // 断开连接时的处理
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
