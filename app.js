39f3d3c14f3ce8836998c77c29c515322b547649
qbbo <qbbo@outlook.sg>
2024-09-30 08:27:19 +0800

    fixed upload

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(middleware.authMiddleware);
-app.use(middleware.uploadMiddleware.single('file'));
 app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
 
